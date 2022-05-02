c9b9e1976aa20a0c13b7371505024db74279eb91
AndreV96av <andreavarallo96@gmail.com>
2022-05-02 18:24:57 +0200

    Esercizio 2

--- Anagrafe.sql
+++ Anagrafe.sql
@@ -2,3 +2,9 @@
 select persona.Nome, città.Regione
 from persona join città on persona.CittàNascita = città.Nome
 where persona.Età >= 18
+
+2.
+select pf.Nome as nome_figlio, pg.Nome as nome_genitore
+from Persona pf join Genia on pf.Nome = Genia.Figlio 
+    join Persona pg on pg.Nome = Genia.Genitore
+where pf.CittàNascita = pg.CittàNascita
