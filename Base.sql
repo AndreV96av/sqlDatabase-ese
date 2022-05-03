b16c1bb050f7940299a21ac79350c10298686e18
AndreV96av <andreavarallo96@gmail.com>
2022-05-03 11:18:23 +0200

    esercizio 2

--- Base.sql
+++ Base.sql
@@ -3,3 +3,10 @@ select programmatore.codice
 from autore join programmatore on programmatore.codice = autore.codice
     join programma on programma.id = autore.id
 where programma.anno > 2000 and programma.linguaggio = "Java"
+
+2.
+select programmatore.nome, programmatore.categoria
+from autore join programmatore on programmatore.codice = autore.codice
+    join programma on programma.id = autore.id
+where programma.linguaggio != "Python"
+order by programmatore.nome
