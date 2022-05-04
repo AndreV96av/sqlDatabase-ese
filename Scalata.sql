14c3526386ae6ba1cfcfdb64130598aa7dec275c
AndreV96av <andreavarallo96@gmail.com>
2022-05-04 11:54:35 +0200

    ese 2

--- Scalata.sql
+++ Scalata.sql
@@ -2,3 +2,10 @@
 select s.scalatore as A, n.nome as B
 from scalata s join nazione n on s.nazione = n.nome
 order by scalatore
+
+2.
+select n.continente, sc.cf
+from scalata s join nazione n on s.nazione = n.nome
+    join scalatore sc on s.scalatore = sc.CF
+where sc.annoNascita < 1980
+order by sc.cf , n.continente
